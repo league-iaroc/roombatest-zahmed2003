4c7bbab458df67a27da470bade98531934125308
The League of Amazing Programmers <league@WTS9.attlocal.net>
2017-04-23 15:57:54 -0700

    23 April 2017

--- src/level4/Challenge.java
+++ src/level4/Challenge.java
@@ -12,17 +12,49 @@ public class Challenge extends Head {
 	public Challenge(Roomba roomba) {
 		super(roomba);
 	}
-	/**
-	 * complete the MAZE
-	 * Hint: use bump sensors
-	 */
-	
-	
-	public void initialize() {
 
+	/**
+	 * complete the MAZE Hint: use bump sensors
+	 */
+
+	public void initialize() {
+		goStraight(2000);
 	}
 
 	public void loop() {
+		if (leftBump) {
+			goBackwards(1000);
+			turnRight(2250);
+			leftBump = false;
+			rightBump = false;
+			goStraight(10000);
+		}
+		if (rightBump) {
+			goBackwards(1000);
+			turnLeft(2250);
+			leftBump = false;
+			rightBump = false;
+			goStraight(10000);
+		}
+	}
 
+	void goStraight(int duration) {
+		driveDirect(500, 500);
+		sleep(duration);
+	}
+
+	void goBackwards(int duration) {
+		driveDirect(-500, -500);
+		sleep(duration);
+	}
+
+	void turnLeft(int duration) {
+		driveDirect(100, 0);
+		sleep(duration);
+	}
+
+	void turnRight(int duration) {
+		driveDirect(0, 100);
+		sleep(duration);
 	}
 }
